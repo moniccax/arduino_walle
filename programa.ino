77ec1913d16ced4e35a64398ff14b990b45578e8
moniccax <41020702+moniccax@users.noreply.github.com>
2022-01-21 11:23:12 +0100

    Add files via upload

---
+++ programa.ino
@@ -0,0 +1,19 @@
+
+// Define o pino analógico A0, onde será lido o valor do sensor
+int sensorAlogPin = 8; 
+
+int armazenaValor = 0;  
+
+void setup(){
+  // Inicializa a função Serial, configurando sua velocidade
+  Serial.begin(9600);  
+}
+
+void loop(){
+// Faz a leitura da porta analogica A0 e armazena na variável armazenaValor
+  armazenaValor = analogRead(sensorAlogPin);  
+  Serial.print("Lendo valor... ");  //   Imprime no monitor serial
+  Serial.print(armazenaValor);  // Mostra o valor lido pela porta serial
+  delay(1000);  // Aguarda 1 segundo
+}
+
